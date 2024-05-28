a85ada6c51c298b53965cf1f1f9150688fe9e883
Sachit <sachitdahal33@gmail.com>
2024-05-28 15:26:55 +0545

    sdj

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,10 +12,10 @@ const config: Config = {
         '13': 'repeat(13, minmax(0, 1fr))',
       },
       colors: {
-        blue: {
-          400: '#2589FE',
-          500: '#0070F3',
-          600: '#2F6FEB',
+        orange: {
+          400: '#FFA500',
+          500: '#FF7F00',
+          600: '#FF8C00',
         },
       },
     },
